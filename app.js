13efdc05ee7ad312a998fd6682a123a239a27c27
livgimenes <77754389+livgimenes@users.noreply.github.com>
2023-01-29 17:20:40 -0500

    updated node references

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/display.html');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
