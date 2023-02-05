477c403f54aef8cfe5695eee5a69344a2506d836
livgimenes <77754389+livgimenes@users.noreply.github.com>
2023-02-05 02:44:36 -0500

    added self updating concurrency

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { spawn } = require('child_process');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -6,6 +7,24 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/display.html');
 });
+
+function runPythonScript() {
+  const pythonScript = spawn('python3', ['/path/to/your/python/script.py']);
+
+  pythonScript.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  pythonScript.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  pythonScript.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
+
+setInterval(runPythonScript, 60 * 60 * 1000);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
