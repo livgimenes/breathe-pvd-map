c2d61575b2c4afb6e863727994ee41f78e8ccb68
livgimenes <77754389+livgimenes@users.noreply.github.com>
2023-02-21 11:03:59 -0500

    tracking added

--- old_data/getcoord.py
+++ old_data/getcoord.py
@@ -9,6 +9,7 @@ from pytz import timezone
 import datetime
 import urllib
 import urllib.parse
+import numpy as np 
 
 ###PRE-CODING READING
 
@@ -22,11 +23,13 @@ def get_requests(node_name, node_id, variable, start_date, start_time, end_date,
                   + "/measurements_all/csv?name=" + str(node_name) + "&interval=60&variables=" + variable + "&start=" +
                   str(start_date) + "%20" + str(start_time) + "&end=" + str(end_date) + "%20" + str(end_time) + "&char_type=measurement")
     
-    print(base_url + custom_url)
     return base_url + custom_url
+
+
 
 def get_requests_for_row(row, start_date, end_date, variable, start_time, end_time):
     """Helper for get_data. Gets requets for a given row, for a pre-defined start-date, end-date, pollution variant and time """
+
 
     url = get_requests(urllib.parse.quote(row["Location"]), row["Node ID"], variable, start_date, start_time, end_date, end_time)
     try:
@@ -35,6 +38,16 @@ def get_requests_for_row(row, start_date, end_date, variable, start_time, end_ti
         data = pd.DataFrame()
         print(f"An error occurred while trying to fetch data from the server for node " + str(row["Node ID"]) + " at " + row["Location"])
     return data
+
+
+#TODO: Comeback to reimplement this 
+def store_failed_requests(failed_requests):
+    #save failed requests to a csv file, add to already existing entries
+    directory = './old_data'
+    file_name = 'failed_requests.csv'
+    file_path = os.path.join(directory, file_name)
+    failed_requests.to_csv(file_path, mode='a', header=False)
+
 
 def get_data(data, start_date, end_date, variable, start_time, end_time):
     """Loads all the measurements from the nodes and store them into a pandas dataframe. To modify specifics go to get_requests for row"""
@@ -59,11 +72,10 @@ def clean_data(data):
   """Cleans the panda dataframe removing missing data, drops unecessary columns and tranforms data from pst to est"""
 
   #drop unecessary columns
-  print(data.head(5))
   data = data.drop(columns=['epoch', 'local_timestamp',"node_file_id"])
 
   #remove missing data, -999
-  data = data.replace({'co2_corrected_avg_t_drift_applied': {-999.00000: pd.NaT}})
+  data = data.replace({'co2_corrected_avg_t_drift_applied': {-999.00000: np.nan}})
   data = data.dropna(subset=['datetime', 'co2_corrected_avg_t_drift_applied'])
 
   #round to no decimals
@@ -107,7 +119,7 @@ def convert_final():
     #change back to the time 
 
     rounded_time = curr_time.replace(minute=0, second=0, microsecond=0)
-    print("this is the rounded time" + str(rounded_time))
+    print("this is the rounded time: " + str(rounded_time))
     start_date = rounded_time.date()
     start_time = "00:00:00"
     #CO2_corrected_avg_t_drift_applied
@@ -115,6 +127,9 @@ def convert_final():
 
 
     data = clean_data(get_data(sensors_df,start_date, end_date, variable, start_time, end_time))
+
+
+
     data = data.rename(columns={'co2_corrected_avg_t_drift_applied': 'co2_corrected'})
     #TODO: Take out
     data.sort_values(by='datetime', ascending=False, inplace=True)
@@ -126,6 +141,8 @@ def convert_final():
     filter_data = str(rounded_time)
 
 
+
+    print("this is the rounded time" + str(rounded_time))
     #it has to be the rounded hour
     print(filter_data)
     
@@ -146,6 +163,9 @@ def convert_final():
     print(combined_data)
 
     #filter to only include 
+
+
+    #have it continously add to a csv file to see what has been displaying
 
     directory = "./public"
     combined_data.to_json(os.path.join(directory, 'coords.json'), orient='records')
