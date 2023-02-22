b3b19acd575e59186b22e2f3db2d9d36655de512
livgimenes <77754389+livgimenes@users.noreply.github.com>
2023-02-21 23:00:42 -0500

    tracking added

--- app.js
+++ app.js
@@ -21,20 +21,13 @@ function runPythonScript() {
   });
 
   pythonScript.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+    console.log(`Json is updated. child process exited with code ${code}`);
   });
 }
 
-function scheduleScript() {
-  var now = new Date();
-  var delay = 60 * 60 * 1000 - (now.getMinutes() * 60 + now.getSeconds()) * 1000 + now.getMilliseconds();
-  setTimeout(function() {
-    runPythonScript();
-    setInterval(runPythonScript, 60 * 60 * 1000);
-  }, delay);
-}
 
-scheduleScript();
+setInterval(runPythonScript, 60 * 1000);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
